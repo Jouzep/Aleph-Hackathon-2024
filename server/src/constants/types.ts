e6f372c6dd46b9e6383f9374b7880393c7a23152
Ethan Nguyen <nahte.neyugn@gmail.com>
2024-02-24 20:47:21 +0100

    feat(controllers): working on product creation endpoint

--- server/src/constants/types.ts
+++ server/src/constants/types.ts
@@ -12,6 +12,11 @@ export type group = {
   products: product[];
 };
 
+interface image {
+  data: ArrayBuffer | Uint8Array;
+  mimeType: string;
+}
+
 export type presetProducts = {
   name: string;
   size: number[];
@@ -20,6 +25,7 @@ export type presetProducts = {
 };
 
 export type product = {
+  image: image;
   name: string;
   description: string;
   price: number;
