049f7d28ec7da7f887b53fc8baea9de8f641fa72
jouzep <josephyu31@gmail.com>
2024-02-24 14:27:03 +0100

    feat(): Post message

--- server/src/aleph/aleph.service.ts
+++ server/src/aleph/aleph.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
-
+import { messages } from 'aleph-sdk-ts';
+import { ImportAccountFromMnemonic } from 'aleph-sdk-ts/dist/accounts/ethereum';
+import {
+  ETHAccount,
+  ImportAccountFromPrivateKey,
+} from 'aleph-sdk-ts/dist/accounts/ethereum';
+import { PostMessage } from 'aleph-sdk-ts/dist/messages/types';
 @Injectable()
-export class AlephService {}
+export class AlephService {
+  constructor() {
+    console.log('AlephService constructor');
+    this.test();
+  }
+
+  async test() {
+    const account = await this.getAccount();
+    console.log('account', account);
+    const res = await this.publishMessage(account);
+    console.log('res', res);
+  }
+  async getAccount(): Promise<ETHAccount> {
+    const account = await ImportAccountFromMnemonic(
+      'describe ring lumber clever salt like medal panther clown budget actress female',
+    );
+    // const key =
+    //   'xprv9ygc1QEEAP5n6ksUfqRx89CLWEH3PAy3BsCmoR8Ch94juX3Kb6f3i9TVvyoTqBidFYFvye7twoNTBgcnVEfFkzfo7wLmkkYvvnnNzfUVBqx';
+    // const account = await ImportAccountFromPrivateKey(key);
+    return account;
+  }
+
+  async publishMessage(
+    account: ETHAccount,
+  ): Promise<PostMessage<{ body: string }>> {
+    const res = await messages.post.Publish({
+      account: account,
+      postType: 'mytype',
+      content: { body: 'content of my post' },
+      channel: 'TEST2405',
+    });
+    return res;
+  }
+}
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AlephModule } from './aleph/aleph.module';
-import { AlpehService } from './alpeh/alpeh.service';
 
 @Module({
   imports: [AlephModule],
   controllers: [AppController],
-  providers: [AppService, AlpehService],
+  providers: [AppService],
 })
 export class AppModule {}
