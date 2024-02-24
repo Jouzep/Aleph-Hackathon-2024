e814799907a8f9111667ff2b7c5b258831d0dcef
Ethan Nguyen <nahte.neyugn@gmail.com>
2024-02-24 23:50:04 +0100

    feat(group-controller): added list of endpoints

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AlephModule } from './aleph/aleph.module';
-import { DictionnaryModule } from './dictionnary/dictionnary.module';
+import { DictionaryModule } from './dictionary/dictionary.module';
 import { GroupService } from './group/group.service';
 import { GroupModule } from './group/group.module';
 
 @Module({
-  imports: [AlephModule, DictionnaryModule, GroupModule],
+  imports: [AlephModule, DictionaryModule, GroupModule],
   controllers: [AppController],
   providers: [AppService, GroupService],
 })
--- server/src/group/group.controller.ts
+++ server/src/group/group.controller.ts
@@ -1,4 +1,29 @@
 import { Controller } from '@nestjs/common';
 
 @Controller('group')
-export class GroupController {}
+export class GroupController {
+  @Get('/ping')
+  pint(): string {
+    return 'pong';
+  }
+
+  @Post('/create')
+  createGroup(): string {
+    return 'Group created';
+  }
+
+  @Get('/list')
+  listGroups(): string {
+    return 'Group list';
+  }
+
+  @Get('/get')
+  getGroup(): string {
+    return 'Group get';
+  }
+
+  @Post('/update')
+  updateGroup(): string {
+    return 'Group updated';
+  }
+}
