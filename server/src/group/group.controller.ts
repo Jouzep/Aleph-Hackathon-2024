30dcb21c70c3cca482ed4fc4d879a9291408c9f3
Ethan Nguyen <nahte.neyugn@gmail.com>
2024-02-25 03:10:26 +0100

    fix(group): fixed module

--- server/src/group/group.controller.ts
+++ server/src/group/group.controller.ts
@@ -1,5 +1,6 @@
 import { Controller } from '@nestjs/common';
 
+@Controller('group')
 export class GroupController {
   constructor(private readonly group: GroupService) {}
 
--- server/src/group/group.module.ts
+++ server/src/group/group.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { GroupController } from './group.controller';
+import { GroupService } from './group.service';
 
 @Module({
-  controllers: [GroupController]
+  controllers: [GroupController],
+  providers: [GroupService]
 })
-export class GroupModule {
-  exports: [GroupModule];
-}
+export class GroupModule {}
